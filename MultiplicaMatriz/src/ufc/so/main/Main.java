c53874a2b1bbfcbf9047728d2da27d7bd52b2ee9
italoneypessoa <italoneypessoa@gmail.com>
2012-04-15 12:26:43 +0000

    alterada para o correto

--- MultiplicaMatriz/src/ufc/so/main/Main.java
+++ MultiplicaMatriz/src/ufc/so/main/Main.java
@@ -12,16 +12,19 @@ import javax.swing.JOptionPane;
  */
 public class Main {
 
-	public static void main(String args[]) {
+    public static void main(String args[]) {
         // o numero de linhas colunas de A tem que ser igualo numero de colunas
         // de B
-
-        int A[][] = new int[3][3];
-        int B[][] = new int[3][2];
-        int C[][] = new int[3][6];
-        int resultado[][] = new int[3][2];
-        // [linha][coluna]
-
+        
+        //linhas e colunas de A e B
+        final int LA = 3,LB = 3;
+        final int CA = 3,CB = 2;
+        
+        int A[][] = new int[LA][CA];
+        int B[][] = new int[LB][CB];
+        int resultado[][] = new int[LA][CB];
+        
+        // <editor-fold defaultstate="collapsed" desc="Valores para teste">  
         A[0][0] = 2;
         A[0][1] = 1;
         A[0][2] = 2;
@@ -34,8 +37,6 @@ public class Main {
         A[2][1] = -1;
         A[2][2] = 2;
 
-        System.err.println("");
-
         B[0][0] = 1;
         B[0][1] = 2;
 
@@ -44,66 +45,36 @@ public class Main {
 
         B[2][0] = -1;
         B[2][1] = 3;
-
+        // </editor-fold> 
+        
         // primeiro percorrer a matriz B, coluna por coluna
-        // multiplicando todos os itens da respsctiva coluna em A pelo valor
-        // atual
-
-        for (int colunaB = 0; colunaB < 2; colunaB++) {
-            for (int linhaB = 0; linhaB < 3; linhaB++) {
-
-                // System.err.print("|"+B[linhaB][colunaB]+"|");
-                int valor = B[linhaB][colunaB];
-
-                for (int z = 0; z < 3; z++) {
-                        //System.out.println(A[z][linhaB] + " * " + valor);
-                    if(colunaB == 1){
-                        //System.out.println("C["+z+"]["+(linhaB+3)+"] = "+A[z][linhaB] + " * " + valor);
-                         C[z][linhaB+3] = A[z][linhaB]* valor;
-                        //System.out.print((A[z][linhaB]* valor) + " ");
-                    }else{
-                        //System.out.println("C["+z+"]["+linhaB+"] = "+A[z][linhaB] + " * " + valor);
-                        C[z][linhaB] = A[z][linhaB]* valor;
-                        //System.out.print((A[z][linhaB]* valor) + " ");
-                    }
+        // multiplicando todos os itens da respectiva coluna em A pelo valor atual
+        
+        // <editor-fold defaultstate="collapsed" desc="Multiplicar matrizes">  
+        for (int colunaB = 0; colunaB < CB; colunaB++) {
+            
+            for (int linhaB = 0; linhaB < LB; linhaB++) {
+                //variavel auxiliar para armazenar valor temporário
+                //para posteriormente adicionar a matriz resultado    
+                int aux =0;
+                //laço para multiplicar os valores
+                for (int x = 0; x < LA; x++) {
+                    aux = aux + (A[linhaB][x] * B[x][colunaB]);
+                    
                 }
+                resultado[linhaB][colunaB] = aux;
             }
-                System.err.println("");
         }
-
-//        for (int x = 0; x < 3; x++) {
-//             for (int y = 0; y < 6; y++) {
-//                    System.out.print(C[x][y] + "|");
-//                }
-//                System.err.println("");
-//        }
-        
-        
-        for (int l = 0; l < 3; l++) {
-            int v1 = 0;
-            int v2 = 0;
-            for (int c = 0; c < 6; c++) {
-                if(c<=2)
-                {
-                    v1+= C[l][c];
-                    System.out.print(v1 +"|");
-                }else{
-                    v2+= C[l][c];
-                    System.err.print(v2 +"|");
-                }
-
+        // </editor-fold>  
+    
+        // <editor-fold defaultstate="collapsed" desc="Imprimir matriz resultado">  
+        for(int l=0; l<LA;l++){
+            for(int c=0; c<CB; c++){
+                System.err.print(resultado[l][c] +"|");
             }
-            System.out.println("");
+            System.err.println("");
         }
-        
+        // </editor-fold>  
 
-        //soma algebrica
-//        for (int c = 0; c < 2; c++) {
-//                for (int l = 0; l < 3; l++) {
-//                    System.err.print(resultado[l][c]);
-//                }
-//                System.out.println("");
-//        }
-
-	}
+    }
 }
