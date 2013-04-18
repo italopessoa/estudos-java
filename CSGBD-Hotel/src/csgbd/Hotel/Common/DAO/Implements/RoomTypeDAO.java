c05d70c00a9e7bdaa3279c9da74d2d893a946cd6
italoneypessoa <italoneypessoa@9118a3ad-a321-070e-9de1-fe4f5642761e>
2013-04-18 16:02:33 +0000

    Correção concluída, verificar se ao realizar o update o valor de data será informado corretamente, pois agora utiliza apenas o nwe Date, e pode ser que o formato esteja incorreto

--- CSGBD-Hotel/src/csgbd/Hotel/Common/DAO/Implements/RoomTypeDAO.java
+++ CSGBD-Hotel/src/csgbd/Hotel/Common/DAO/Implements/RoomTypeDAO.java
@@ -8,6 +8,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -78,23 +79,35 @@ public class RoomTypeDAO implements IDAO<RoomType> {
     }
 
     @Override
-    public void Update(RoomType object) {
-        RoomType roomType;
-        if (object.getClass().equals(RoomType.class)) {
-            roomType = (RoomType) object;
-        } else {
-            throw new ClassCastException("O valor informado não corresponde ao esperado!");
-        }
+    public void Update(RoomType roomType) {
         try {
+            
+            HashMap<String, Integer> indexMap = new HashMap<String, Integer>();
+            int auxCount = 2;
+            StringBuilder sb = new StringBuilder("update roomtype set dtupdate=? ");
+            
+            if(roomType.getName()!= null && !roomType.getName().equals("")){
+                indexMap.put("RoomTypeName", auxCount);
+                sb.append(" ,RoomTypeName = ? ");
+                auxCount++;
+            }
+            
+            indexMap.put("idroomtype", auxCount);
+            sb.append(" where idroomtype = ? ");
+                
             String query = "update roomtype set RoomTypeName = ?,DtUpdate = ? where idroomtype = ?;";
-            PreparedStatement ps = ConnectionManager.PreparedStatement(query);
-            ps.setString(1, roomType.getName());
-            Date actualDate = new Date();
-            //mes 0 = 12
-            ps.setDate(2, java.sql.Date.valueOf(String.format("%3$d-%2$d-%1$d",
-                    actualDate.getDate(), actualDate.getMonth(), actualDate.getYear() + 1900)));
-
-            ps.setInt(3, roomType.getId());
+            
+            PreparedStatement ps = ConnectionManager.PreparedStatement(sb.toString());
+            
+            if(indexMap.containsKey("RoomTypeName")){
+                ps.setString(indexMap.get("RoomTypeName"), roomType.getName());
+            }
+            
+            if(indexMap.containsKey("idroomtype")){
+                ps.setInt(indexMap.get("idroomtype"), roomType.getId());
+            }
+            
+            ps.setDate(1, (java.sql.Date)new Date());
             ps.executeUpdate();
         } catch (SQLException ex) {
             Logger.getLogger(RoomTypeDAO.class.getName()).log(Level.SEVERE, null, ex);
