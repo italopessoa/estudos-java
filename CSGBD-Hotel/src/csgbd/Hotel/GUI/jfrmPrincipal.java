a0b96351210a538ed728e7cae2e14dce657d00aa
italoneypessoa <italoneypessoa@9118a3ad-a321-070e-9de1-fe4f5642761e>
2013-04-18 23:45:31 +0000

    Formulário principal

--- CSGBD-Hotel/src/csgbd/Hotel/GUI/jfrmPrincipal.java
+++ CSGBD-Hotel/src/csgbd/Hotel/GUI/jfrmPrincipal.java
@@ -2,17 +2,18 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
+package csgbd.Hotel.GUI;
 
 /**
  *
  * @author italopessoa
  */
-public class NewJFrame extends javax.swing.JFrame {
+public class jfrmPrincipal extends javax.swing.JFrame {
 
     /**
-     * Creates new form NewJFrame
+     * Creates new form jfrmPrincipal
      */
-    public NewJFrame() {
+    public jfrmPrincipal() {
         initComponents();
     }
 
@@ -58,20 +59,20 @@ public class NewJFrame extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(jfrmPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(jfrmPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(jfrmPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(jfrmPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new NewJFrame().setVisible(true);
+                new jfrmPrincipal().setVisible(true);
             }
         });
     }
