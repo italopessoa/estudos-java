acda06417e7403473ddb321713fb69cdba99a466
italoneypessoa <italoneypessoa@gmail.com>
2013-04-18 01:38:59 +0000

    Replicar correção de Update nas demais classes de DAO

--- CSGBD-Hotel/src/csgbd/Hotel/Common/DAO/Implements/RoomTypeDAO.java
+++ CSGBD-Hotel/src/csgbd/Hotel/Common/DAO/Implements/RoomTypeDAO.java
@@ -15,14 +15,14 @@ import java.util.logging.Logger;
  *
  * @author italopessoa
  */
-public class RoomTypeDAO implements IDAO<RoomType>{
+public class RoomTypeDAO implements IDAO<RoomType> {
 
     @Override
     public void Save(RoomType object) {
         RoomType roomType;
-        if(object.getClass().equals(RoomType.class)){
-            roomType = (RoomType)object;
-        }else{
+        if (object.getClass().equals(RoomType.class)) {
+            roomType = (RoomType) object;
+        } else {
             throw new ClassCastException("O valor informado não corresponde ao esperado!");
         }
         try {
@@ -31,8 +31,8 @@ public class RoomTypeDAO implements IDAO<RoomType>{
             ps.setString(1, roomType.getName());
             Date actualDate = new Date();
             //mes 0 = 12
-            ps.setDate(2, java.sql.Date.valueOf(String.format("%3$d-%2$d-%1$d", 
-                    actualDate.getDate(),actualDate.getMonth(),actualDate.getYear()+1900)));
+            ps.setDate(2, java.sql.Date.valueOf(String.format("%3$d-%2$d-%1$d",
+                    actualDate.getDate(), actualDate.getMonth(), actualDate.getYear() + 1900)));
             ps.executeUpdate();
         } catch (SQLException ex) {
             Logger.getLogger(RoomTypeDAO.class.getName()).log(Level.SEVERE, null, ex);
@@ -42,9 +42,9 @@ public class RoomTypeDAO implements IDAO<RoomType>{
     @Override
     public void Delete(RoomType object) {
         RoomType roomType;
-        if(object.getClass().equals(RoomType.class)){
-            roomType = (RoomType)object;
-        }else{
+        if (object.getClass().equals(RoomType.class)) {
+            roomType = (RoomType) object;
+        } else {
             throw new ClassCastException("O valor informado não corresponde ao esperado!");
         }
         try {
@@ -59,12 +59,12 @@ public class RoomTypeDAO implements IDAO<RoomType>{
 
     @Override
     public ArrayList<RoomType> SelectAll() {
-        ArrayList roomTypes = new ArrayList();
+        ArrayList<RoomType> roomTypes = new ArrayList<RoomType>();
         String sql_str = "SELECT * FROM roomtype";
         RoomType roomType;
         try {
             ResultSet rs = ConnectionManager.GetStatement().executeQuery(sql_str);
-            while(rs.next()){
+            while (rs.next()) {
                 roomType = new RoomType();
                 roomType.setId(rs.getInt("IdRoomType"));
                 roomType.setName(rs.getString("RoomTypeName"));
@@ -80,9 +80,9 @@ public class RoomTypeDAO implements IDAO<RoomType>{
     @Override
     public void Update(RoomType object) {
         RoomType roomType;
-        if(object.getClass().equals(RoomType.class)){
-            roomType = (RoomType)object;
-        }else{
+        if (object.getClass().equals(RoomType.class)) {
+            roomType = (RoomType) object;
+        } else {
             throw new ClassCastException("O valor informado não corresponde ao esperado!");
         }
         try {
@@ -91,9 +91,9 @@ public class RoomTypeDAO implements IDAO<RoomType>{
             ps.setString(1, roomType.getName());
             Date actualDate = new Date();
             //mes 0 = 12
-            ps.setDate(2, java.sql.Date.valueOf(String.format("%3$d-%2$d-%1$d", 
-                    actualDate.getDate(),actualDate.getMonth(),actualDate.getYear()+1900)));
-            
+            ps.setDate(2, java.sql.Date.valueOf(String.format("%3$d-%2$d-%1$d",
+                    actualDate.getDate(), actualDate.getMonth(), actualDate.getYear() + 1900)));
+
             ps.setInt(3, roomType.getId());
             ps.executeUpdate();
         } catch (SQLException ex) {
